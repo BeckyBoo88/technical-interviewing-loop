f1a492a7e5bd32f32fe2627f2e852f31a864d4f1
BeckyBoo88 <rbooco07@gmail.com>
2021-07-23 17:03:45 -0600

    solved first one, kinda have 3

---
+++ coding-challenge/challenge-1.language_of_choice.py
@@ -0,0 +1,9 @@
+def first_lower(s):
+   if len(s) == 0:
+      return s
+   else:
+      return s.swapcase()
+
+
+
+print (first_lower("Hello World"))  
---
+++ script.js
@@ -0,0 +1,17 @@
+// **Full credit to @Emma Bostian for this question.**
+
+// Using the [Random User Generator API](https://randomuser.me/), query for a list of 10 users and for each user and return an array of objects with their profile photo and name.
+
+// Write your script in Javascript, and submit in a file called `script.js`.
+
+fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(randomData => {
+        console.log(randomData)
+    })
+
+{/* <div>
+    {randomData.data.results.name.first}
+    {randomData.data.results.name.last}
+    {randomData.data.results.picture.medium}
+</div> */}
